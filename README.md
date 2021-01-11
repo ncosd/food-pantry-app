2841af82493c776ecf1adfa3adb2c0988c4ad8e2
Darrel Herbst <dherbst@gmail.com>
2021-01-11 14:37:27 -0500

    A simple application with registration and login.

--- README.md
+++ README.md
@@ -12,3 +12,15 @@ The Food Pantry application is designed to:
 
 ## Project Roadmap
 The project uses Github projects for its roadmap, you can find it here: https://github.com/ncosd/food-pantry-app/projects/1
+
+## Developing
+This project uses firebase and Vue.js.  It does not have a firebase project configured.  To run this you will need to use the firebase emulators locally.
+
+You can run this project locally against the firebase emulators.  You can see how to install and configure the firebase emulators here https://firebase.google.com/docs/emulator-suite/install_and_configure.
+
+To run the project locally, clone this repo and then go into the `app` directory:
+
+    npm run server:firebase:emulator  # leave this running in one terminal
+    npm run build:watch               # leave this running in another terminal
+
+Running these two commands will run the emulators in one terminal, and the vue-cli-service in the other.  The vue service will not do hot-reloading, it will rebuild to the `dist` folder whenever changes are made, so you will need to reload the web browser as you work.
---
+++ app/src/views/Home.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="home">
+    <v-card elevation="2" max-width="90%" class="mx-auto my-12">
+      <v-card-title>Welcome!</v-card-title>
+      <template v-if="user.loggedIn">
+      <v-card-text>
+      <p>You are logged in.  All registered users are considered Guests of the Food Bank.</p>
+      <p>If you would like to sign up for delivery, please fill out this form ...</p>
+      <p>If you would like to volunteer, please fill out this form...</p>
+      </v-card-text>
+      </template>
+      <template v-else>
+      <v-card-text>
+      Welcome to the Narberth Community Food Bank app.  You can register with the application and choose to be a Guest or a Volunteer.  Once you have
+      registered, use your profile to customize how you interact with the Food Bank.
+      </v-card-text>
+      </template>
+    </v-card>
+
+    <v-card elevation="2" max-width="90%" class="mx-auto my-12">
+      <v-card-title>Items we need</v-card-title>
+      <v-card-text v-html="neededList">
+      {{neededList}}
+      </v-card-text>
+    </v-card>
+
+
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'Home',
+  computed: {
+    ...mapGetters({
+    user: "user"
+    })
+  },
+  data() {
+    return {
+      neededList: "loading..."
+
+    }
+  },
+  mounted() {
+    const db = firebase.firestore();
+    db.collection('needed').doc('latest').get().then( (docRef) => {
+      this.neededList = docRef.data().text;
+    }).catch(err => {
+      console.log(err);
+      this.neededList = 'Something is wrong, please try again.';
+    });
+  }
+}
+</script>
