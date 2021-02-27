f997e5dd3f18251b81598b4d295ab329a33d11da
Darrel Herbst <dherbst@gmail.com>
2021-02-27 16:49:17 -0500

    Added validation for delivery form.

--- app/src/plugins/vuetify.js
+++ app/src/plugins/vuetify.js
@@ -1,17 +1,17 @@
 import Vue from 'vue';
-//import Vuetify from 'vuetify';
+import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
-import { Vuetify, VApp, VForm } from 'vuetify/lib';
+//import { Vuetify, VApp, VForm } from 'vuetify/lib';
 
-// //import 'vuetify/src/stylus/app.styl';
-import 'material-design-icons-iconfont/dist/material-design-icons.css';
+//import 'vuetify/src/stylus/app.styl';
+//import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 Vue.use(Vuetify, {
   iconfont: 'md'
 })
 
-Vue.component('v-app', VApp)
-Vue.component('v-form', VForm)
+//Vue.component('v-app', VApp)
+//Vue.component('v-form', VForm)
 
 // const req = require.context('../../src/stories', true, /.stories.js$/)
 
