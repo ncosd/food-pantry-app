5792b4daf11fac90d3058db300e62ef3cf916067
Darrel Herbst <dherbst@gmail.com>
2021-03-04 11:22:44 -0500

    For #31 create framework for setting page title and other SEO metadata.

--- app/src/config.js
+++ app/src/config.js
@@ -4,5 +4,53 @@ this.config = {
   ProjectLongName: process.env.VUE_APP_PROJECT_LONG_NAME || 'PROJECT_LONG_NAME',
   DeliveryAreaNames: process.env.VUE_APP_DELIVERY_AREA_NAMES || "Narberth and Lower Merion Township",
   DeliveryZipcodes: process.env.VUE_APP_DELIVERY_ZIP_CODES || [19003, 19010, 19041, 19072, 19096, 19004, 19035, 19066, 19085, 19428],
-  DeliveryOutsideAreaUrl: process.env.VUE_APP_DELIVERY_OUTSIDE_AREA_URL || "https://connect4health.org/"
+  DeliveryOutsideAreaUrl: process.env.VUE_APP_DELIVERY_OUTSIDE_AREA_URL || "https://connect4health.org/",
+
+  meta: {
+    Home: {
+      title: 'Home',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'Home page'
+        }
+      ]
+    },
+    Login: {
+      title: 'Login',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'Login'
+        }
+      ]
+    },
+    Register: {
+      title: 'Register',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'Register for an account'
+        }
+      ]
+    },
+    About: {
+      title: 'About',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'About page'
+        }
+      ]
+    },
+    DeliveryForm: {
+      title: 'Delivery Profile',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'Your delivery application/profile page.'
+        }
+      ]
+    }
+  }
 };
--- app/src/router/index.js
+++ app/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
+import { config } from '@/config'
 
 Vue.use(VueRouter)
 
@@ -10,17 +11,20 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: config.meta.Home
   },
   {
     path: '/login',
     name: 'login',
-    component: Login
+    component: Login,
+    meta: config.meta.Login
   },
   {
     path: '/register',
     name: 'register',
-    component: Register
+    component: Register,
+    meta: config.meta.Register
   },
   {
     path: '/about',
@@ -28,12 +32,15 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    meta: config.meta.About
+
   },
   {
     path: '/delivery-form',
     name: 'DeliveryForm',
-    component: () => import(/* webpackChunkName: "delivery-form" */ '../views/DeliveryForm.vue')
+    component: () => import(/* webpackChunkName: "delivery-form" */ '../views/DeliveryForm.vue'),
+    meta: config.meta.DeliveryForm
   }
 ]
 
@@ -42,5 +49,44 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+
+// This callback runs before every route change, including on page load.
+router.beforeEach((to, from, next) => {
+  // This goes through the matched routes from last to first, finding the closest route with a title.
+  // e.g., if we have `/some/deep/nested/route` and `/some`, `/deep`, and `/nested` have titles,
+  // `/nested`'s will be chosen.
+  const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title);
+
+  // Find the nearest route element with meta tags.
+  const nearestWithMeta = to.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
+
+  // If a route with a title was found, set the document (page) title to that value.
+  if(nearestWithTitle) document.title = nearestWithTitle.meta.title;
+
+  // Remove any stale meta tags from the document using the key attribute we set below.
+  Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode.removeChild(el));
+
+  // Skip rendering meta tags if there are none.
+  if(!nearestWithMeta) return next();
+
+  // Turn the meta tag definitions into actual elements in the head.
+  nearestWithMeta.meta.metaTags.map(tagDef => {
+    const tag = document.createElement('meta');
+
+    Object.keys(tagDef).forEach(key => {
+      tag.setAttribute(key, tagDef[key]);
+    });
+
+    // We use this to track which meta tags we create so we don't interfere with other ones.
+    tag.setAttribute('data-vue-router-controlled', '');
+
+    return tag;
+  })
+  // Add the meta tags to the document head.
+  .forEach(tag => document.head.appendChild(tag));
+
+  next();
+});
+
 
 export default router
