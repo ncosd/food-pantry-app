f9cc21de339736235ee0412730630ee54e70ae6e
Darrel Herbst <dherbst@gmail.com>
2021-03-07 10:40:53 -0500

    Added Footer with links to terms and privacy policy.

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <v-app>
-  <v-main>
-    <app-navigation></app-navigation>
-    <router-view></router-view>
-   </v-main>
+    <v-main>
+      <app-navigation></app-navigation>
+      <router-view></router-view>
+      <Footer></Footer>
+     </v-main>
   </v-app>
 </template>
 
 <script>
 import AppNavigation from '@/components/AppNavigation'
+import Footer from '@/components/Footer'
 
 export default {
   name: 'App',
   components: {
-    AppNavigation
+    AppNavigation,
+    Footer
   }
 }
 </script>
---
+++ app/src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-footer color="blue darken-4" padless>
+    <v-card flat tile class="blue darken-4 white--text text-left">
+      <v-card-text x-small class="white--text">
+      <v-btn x-small color="white" flat to="/terms" text>Terms</v-btn>
+      <v-btn x-small color="white" flat to="/privacy-policy" text>Privacy Policy</v-btn>
+      <v-btn x-small color="white" flat text href="https://github.com/ncosd/food-pantry-app">Made with Food-Pantry-App</v-btn>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style>
+</style>
