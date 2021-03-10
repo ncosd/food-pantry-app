eeebf047805be709d4fddaa490ec19ab18e1c4c4
Darrel Herbst <dherbst@gmail.com>
2021-03-10 08:49:57 -0500

    Removed logging.

--- app/src/views/ForgotPassword.vue
+++ app/src/views/ForgotPassword.vue
@@ -28,18 +28,15 @@ export default {
   },
   methods: {
     onFormSubmit(email) {
-       console.log('forgot ', email);
        var auth = firebase.auth();
        auth.sendPasswordResetEmail(email).then(() => {
-          console.log('success');
           this.showError = false;
           this.showSuccess = true;
           this.successMessage = 'Email sent with a link to reset your password.  Please check you spam folder if you do not see it in a few minutes.';
-       }).catch((error)=>{
+       }).catch(() => {
           this.showSuccess = false;
           this.showError = true;
           this.errorMessage = 'An error occurred.  Check you entered the correct email for your account.';
-          console.log(error);
        });
     }
   }
