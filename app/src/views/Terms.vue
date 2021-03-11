2a31c2a7a2c18677a3ae74dba9db86ad5a96893a
Darrel Herbst <dherbst@gmail.com>
2021-03-11 16:17:48 -0500

    Updated to read terms from firestore.

--- app/src/views/Terms.vue
+++ app/src/views/Terms.vue
@@ -1,10 +1,41 @@
 <template>
- <h1>Terms of Use</h1>
-
+  <div>
+    <template v-if="termHtml == ''">
+       <v-card elevation="0" max-width="90%">
+         <v-card-text>Terms missing, please set in firestore.</v-card-text>
+       </v-card>
+    </template>
+    <template v-else>
+      <v-card elevation="0" max-width="90%">
+        <v-card-text v-html="termHtml"></v-card-text>
+      </v-card>
+    </template>
+  </div>
 </template>
 
 <script>
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
+export default {
+  name: 'Terms',
+  data() {
+    return {
+      termHtml: ""
+    }
+  },
+  created() {
+    const db = firebase.firestore();
+    db.collection('terms').doc('latest').get().then( (docRef) => {
+       if (docRef && docRef.data()) {
+         this.termHtml = docRef.data().html;
+       }
+    }).catch(err => {
+       console.log(err);
+       this.termHtml = 'Something is wrong, please try again.';
+    });
+  }
+};
 </script>
 
 <style></style>
