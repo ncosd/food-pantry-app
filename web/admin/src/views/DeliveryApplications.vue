a986ad8d7f515bf89f134e850aa9367a4692ab9d
Darrel Herbst <dherbst@gmail.com>
2021-04-10 08:57:24 -0400

    Loading deliveries from firebase.

--- web/admin/src/views/DeliveryApplications.vue
+++ web/admin/src/views/DeliveryApplications.vue
@@ -7,6 +7,7 @@
 
 <script>
 import Deliveries from '@/components/Deliveries'
+import firebase from 'firebase/app';
 
 export default {
   name: 'DeliveryApplications',
@@ -16,99 +17,24 @@ export default {
 
   data() {
     return {
-      deliveries: [
-        {
-          firstname: 'One',
-          lastname: 'last',
-          userid: '1',
-          address1: '201 Sabine Ave',
-          address2: 'address2',
-          city: 'narberth',
-          state: 'pa',
-          zip: '19072',
-          phone: '610-555-1111',
-          num_10: '1',
-          num_1017: '1',
-          num_1859: '1',
-          num_60: '0',
-          created: '2021-03-09',
-          status: 'in-review',
-          updated: '2021-03-09',
-        },
-        {
-          firstname: 'Two',
-          lastname: 'Last2',
-          userid: '1',
-          address1: '101 Sabine Ave',
-          address2: 'address2',
-          city: 'Ardmore',
-          state: 'pa',
-          zip: '19004',
-          phone: '610-555-1112',
-          num_10: '1',
-          num_1017: '1',
-          num_1859: '1',
-          num_60: '0',
-          created: '2021-03-09',
-          status: 'in-review',
-          updated: '2021-03-09',
-        },
-        {
-          firstname: 'three',
-          lastname: 'last3',
-          userid: '1',
-          address1: '201 Sabine Ave',
-          address2: 'address2',
-          city: 'Bryn Mawr',
-          state: 'pa',
-          zip: '19010',
-          phone: '610-555-1113',
-          num_10: '1',
-          num_1017: '1',
-          num_1859: '1',
-          num_60: '0',
-          created: '2021-03-09',
-          status: 'in-review',
-          updated: '2021-03-09',
-        },
-        {
-          firstname: 'four',
-          lastname: 'last',
-          userid: '1',
-          address1: '201 Sabine Ave',
-          address2: 'address2',
-          city: 'narberth',
-          state: 'pa',
-          zip: '19072',
-          phone: '610-555-1114',
-          num_10: '1',
-          num_1017: '1',
-          num_1859: '1',
-          num_60: '0',
-          created: '2021-03-09',
-          status: 'in-review',
-          updated: '2021-03-09',
-        },
-        {
-          firstname: 'five',
-          lastname: 'last',
-          userid: '1',
-          address1: '105 Sabine Ave',
-          address2: 'address2',
-          city: 'narberth',
-          state: 'pa',
-          zip: '19072',
-          phone: '610-555-1115',
-          num_10: '1',
-          num_1017: '1',
-          num_1859: '1',
-          num_60: '0',
-          created: '2021-03-09',
-          status: 'in-review',
-          updated: '2021-03-09',
-        }
-      ]
+      deliveries: []
     }
+  },
+  created() {
+    const db = firebase.firestore()
+    db.collection('deliveryprofile').get().then( (deliveryQuery) => {
+      let deliveries = []
+      deliveryQuery.forEach(doc => {
+        let d = doc.data()
+        d.id = doc.id
+        deliveries.push(d)
+      })
+
+      this.deliveries = deliveries
+    }).catch((error) => {
+      console.log('Error getting deliveries ', error)
+    })
+
   }
 }
 
