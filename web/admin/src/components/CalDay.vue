6745e7ebb4468d03e639bb6cc571f7e236f00b62
Darrel Herbst <dherbst@gmail.com>
2023-04-07 17:08:54 -0400

    Adds storybook for calendar day component

--- web/admin/src/components/CalDay.vue
+++ web/admin/src/components/CalDay.vue
@@ -1,7 +1,18 @@
 <template>
-  <div class="col border text-secondary">
+  <div class="col border text-secondary cal-day">
     {{date.getDate()}}
     <div><a href="/volunteer/admin/cal-new/?d={{date.toISOString()}}" class="btn btn-secondary btn-sm">+</a></div>
+    <template v-for="w in windows">
+    <div class="badge rounded-pill text-wrap d-block m-1 text-bg-secondary"
+    :class="{ 'text-bg-secondary': isCovered(w.status), 'text-bg-warning': isOpen(w.status), 'text-bg-danger': isCancelled(w.status) }">
+    {{ formatHour(w.start) }} - {{ formatHour(w.end) }} {{ w.status }}
+    {{ isCovered(w.status) }}
+    {{ isOpen(w.status) }}
+    {{ isCancelled(w.status) }}
+
+    </div>
+    </template>
+
 
   </div>
 </template>
@@ -10,10 +21,40 @@
 
 export default {
   name: 'CalDay',
-  data() {
-    return {
-      date: new Date()
-    }
+  props: {
+    date: {
+      type: Object,
+    },
+    windows: {
+      type: Array,
+    },
+  },
+  methods: {
+    formatHour(date) {
+      let hour = date.getHours()
+      let meridian = 'am'
+      if (hour > 12) {
+        meridian = 'pm'
+        hour -= 12
+      }
+      return hour + ' ' + meridian
+    },
+    badgeText(status) {
+       let statusClass = 'text-bg-secondary'
+       if (status === 'open') {
+         statusClass='text-bg-warning'
+       }
+       return statusClass
+    },
+    isOpen(status) {
+      return status === 'open'
+    },
+    isCovered(status) {
+      return status === 'covered'
+    },
+    isCancelled(status) {
+      return status === 'cancelled'
+    },
   }
 }
 </script>
--- web/admin/src/stories/CalDay.stories.js
+++ web/admin/src/stories/CalDay.stories.js
@@ -14,15 +14,42 @@ export default {
         ...args,
       };
     },
-    template: '<cal-day :date="date" />',
+    template: '<cal-day :date="date" :windows="windows" />',
   }),
   parameters: {
     layout: 'fullscreen',
   },
 };
 
+const startDate = new Date();
+const endDate = new Date();
+endDate.setTime(startDate.getTime()+3000*60*60);
+let s1 = new Date(endDate);
+let e1 = new Date()
+e1.setTime(s1.getTime()+3000*60*60);
+
+
+
 export const Today = {
   args: {
     date: new Date(),
+    windows: [
+      {
+        start: startDate,
+        end: endDate,
+        status: "covered",
+      },
+      {
+        start: s1,
+        end: e1,
+        status: "open",
+      },
+      {
+        start: s1,
+        end: e1,
+        status: "cancelled",
+      }
+
+      ]
   },
 };
