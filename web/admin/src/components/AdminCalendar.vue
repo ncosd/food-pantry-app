1babfd033ccf073eb856ba8487f390a9de16a2ca
Darrel Herbst <dherbst@gmail.com>
2023-04-08 09:02:27 -0400

    Adds AdminCalendar component.

---
+++ web/admin/src/components/AdminCalendar.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="mt-3">
+  <div class="row border text-center">
+    <h2>{{ formatMonth(date) }} {{date.getFullYear()}}</h2>
+  </div>
+</div>
+</template>
+
+
+<script>
+export default {
+  name: 'AdminCalendar',
+  props: {
+    date: {
+      type: Object,
+    },
+  },
+  methods: {
+    formatMonth(date) {
+      const months = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+      return months[date.getMonth()];
+    },
+  }
+}
+</script>
