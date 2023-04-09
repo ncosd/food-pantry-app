7b84de5de155d8b3860cf21c36ae4f8e943c4e3f
Darrel Herbst <dherbst@gmail.com>
2023-04-09 10:21:03 -0400

    Adds calendar rendering.

---
+++ web/admin/src/components/WeekHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="row text-center">
+    <div class="col border d-none d-md-block">Sun</div>
+    <div class="col border">Mon</div>
+    <div class="col border">Tue</div>
+    <div class="col border">Wed</div>
+    <div class="col border">Thu</div>
+    <div class="col border">Fri</div>
+    <div class="col border d-none d-md-block">Sat</div>
+  </div>
+</template>
