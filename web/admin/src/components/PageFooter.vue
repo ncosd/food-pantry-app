1fc4d8fb605377e53c1e1ff3b2065bae506f3465
Darrel Herbst <dherbst@gmail.com>
2023-04-20 09:06:48 -0400

    Adds footer to admin app.

---
+++ web/admin/src/components/PageFooter.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <footer class="footer my-3">
+    <ul class="list-group list-group-horizontal border border-0">
+      <li class="list-group-item border border-0"><router-link to="/terms">Terms</router-link></li>
+      <li class="list-group-item border border-0"><router-link to="/privacy-policy">Privacy Policy</router-link></li>
+      <li class="list-group-item border border-0"><router-link to="/contact"><i class="bi bi-telephone-fill"></i> Contact</router-link></li>
+      <li class="list-group-item border border-0"><a href="https://github.com/ncosd/food-pantry-app"><i class="bi bi-github"></i> Made with Food-Pantry-App</a></li>
+    </ul>
+  </footer>
+</template>
