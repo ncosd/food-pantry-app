5dd49b1361cd5c9fa2a3dc16f090c929fb29ad75
Darrel Herbst <dherbst@gmail.com>
2023-04-22 13:47:33 -0400

    Fixes #124 for location crud

--- web/admin/src/views/LocationPage.vue
+++ web/admin/src/views/LocationPage.vue
@@ -1,90 +1,104 @@
 <script setup>
 import { ref, defineProps, onBeforeMount } from 'vue'
+import { useRouter } from 'vue-router'
 import { useAuthUserStore } from '@/stores/authUser'
-import { collection, getFirestore, query, where, doc, getDocs, addDoc, updateDoc } from 'firebase/firestore'
+import { collection, getFirestore, query, where, doc, getDoc, addDoc, updateDoc, deleteDoc } from 'firebase/firestore'
 
 const props = defineProps({
   id: String,
 })
 const user = useAuthUserStore()
-const locations = ref()
+var location = ref()
+var showSaveMessage = ref(false)
+var showDeleteMessage = ref(false)
+var saveMessage = ref('Location Saved')
+var deleteMessage = ref('Location deleted')
 
-var isSuccess = ref(false)
-var isError = ref(false)
-var errMessage = ref('')
-var createName = ref('')
-var createStreet = ref('')
-var createCity = ref('')
-var createState = ref('')
-var createZip = ref('')
-const createLocation = ( async ()=>{
-  console.log('createLocation', createName.value, createStreet.value, createCity.value, createState.value, createZip.value)
-  const locdata = { name: createName.value, street: createStreet.value, city: createCity.value, state: createState.value, zip: createZip.value }
+const router = useRouter()
+
+const resetShowMessages = (()=>{
+  showSaveMessage.value = false
+  showDeleteMessage.value = false
+})
+
+const deleteLocation = ( async ()=> {
+  console.log('deleteLocation', props.id)
+  resetShowMessages()
   const db = getFirestore()
-  const locDocRef = await addDoc(collection(db, 'location'), locdata)
-  console.log('saved location', locDocRef.id)
+  await deleteDoc(doc(db, "location", props.id))
+  showDeleteMessage.value = true
+  router.replace({name: 'LocationsList'})
+})
+
+const createLocation = ( async ()=>{
+  resetShowMessages()
+  if (props.id !== null) {
+    console.log('update Location', location.value.name, location.value.street, location.value.city, location.value.state, location.value.zip)
+    const db = getFirestore()
+    const locDocRef = await doc(db, 'location', props.id)
+    await updateDoc(locDocRef, location.value)
+    showSaveMessage.value = true
+    console.log('updated location', locDocRef.id)
+
+  } else {
+    console.log('createLocation', location.value.name, location.value.street, location.value.city, location.value.state, location.value.zip)
+    const db = getFirestore()
+    const locDocRef = await addDoc(collection(db, 'location'), location.value)
+    console.log('saved location', locDocRef.id)
+  }
 })
 
 const clearCreate = ()=>{
   console.log('clear')
-  createName.value = ""
-  createStreet.value = ""
-  createCity.value = ""
-  createState.value = ""
-  createZip.value = ""
+  location.value.name = ""
+  location.value.street = ""
+  location.value.city = ""
+  location.value.state = ""
+  location.value.zip = ""
 }
 
 onBeforeMount( async () => {
-  const db = getFirestore()
-  const q = query(collection(db, "location"))
-  const locRef = await getDocs(q)
-  const locarray = []
-  locRef.forEach((loc)=> {
-    locarray.push(loc.data())
-  })
-  locations.value = locarray
-
+  if (props.id) {
+    console.log('onBeforeMount id=',props.id)
+    const db = getFirestore()
+    const locRef = doc(db, 'location', props.id)
+    const locSnap = await getDoc(locRef)
+    if (locSnap.exists()) {
+      location.value = locSnap.data()
+    }
+  }
 })
-
-var showCreate = ref(false)
-
-const toggleCreate = ( ()=>{
-  showCreate.value = !showCreate.value
-})
-
 </script>
 
 <template>
 <div class="container">
          <form @submit.prevent="createLocation">
+           <div v-if="showSaveMessage" class="text-bg-success">{{saveMessage}}</div>
+           <div v-if="showDeleteMessage" class="text-bg-danger">{{deleteMessage}}</div>
+
            <div class="row">
-             <div v-if="nameError" class="text-bg-danger">Name validation error</div>
              <label class="form-label" for="createLocationName">Location Name</label>
-             <input id="createLocationName" type="text" class="form-control" v-model="createName" required>
+             <input id="createLocationName" type="text" class="form-control" v-model="location.name" required>
            </div>
 
            <div class="row">
-             <div v-if="streetError" class="text-bg-danger">Street validation error</div>
              <label class="form-label" for="createStreet">Street</label>
-             <input id="createStreet" type="text" class="form-control" v-model="createStreet" required>
+             <input id="createStreet" type="text" class="form-control" v-model="location.street" required>
            </div>
 
            <div class="row">
-             <div v-if="cityError" class="text-bg-danger">Name validation error</div>
              <label class="form-label" for="createCity">City</label>
-             <input id="createCity" type="text" class="form-control" v-model="createCity" required>
+             <input id="createCity" type="text" class="form-control" v-model="location.city" required>
            </div>
 
            <div class="row">
-             <div v-if="nameError" class="text-bg-danger">Name validation error</div>
              <label class="form-label" for="createState">State</label>
-             <input id="createState" type="text" class="form-control" v-model="createState" required>
+             <input id="createState" type="text" class="form-control" v-model="location.state" required>
            </div>
 
            <div class="row">
-             <div v-if="nameError" class="text-bg-danger">Name validation error</div>
              <label class="form-label" for="createZip">Zip</label>
-             <input id="createZip" type="text" class="form-control" v-model="createZip" required>
+             <input id="createZip" type="text" class="form-control" v-model="location.zip" required>
            </div>
 
            <div class="mt-3">
@@ -93,7 +107,7 @@ const toggleCreate = ( ()=>{
                  <button type="submit" class="btn btn-primary btn-sm">Save</button>
                </div>
                <div class="col text-end">
-                 <button class="btn btn-danger btn-sm">Delete</button>
+                 <button @click.prevent="deleteLocation" class="btn btn-danger btn-sm">Delete</button>
                </div>
              </div>
            </div>
