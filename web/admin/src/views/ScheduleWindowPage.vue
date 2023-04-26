3a08f6ed393ffc5e4087bf4ff0f0535953002601
Darrel Herbst <dherbst@gmail.com>
2023-04-26 16:30:13 -0400

    Design for windows and covering.

--- web/admin/src/views/ScheduleWindowPage.vue
+++ web/admin/src/views/ScheduleWindowPage.vue
@@ -1,7 +1,8 @@
 <script setup>
 import { ref, defineProps, onMounted } from 'vue'
-import VueDatePicker from '@vuepic/vue-datepicker';
+import VueDatePicker from '@vuepic/vue-datepicker'
 import '@vuepic/vue-datepicker/dist/main.css'
+import dayjs from 'dayjs'
 
 
 const props = defineProps({
@@ -17,20 +18,39 @@ scheduleDate.setMinutes(0)
 
 const windowDate = ref()
 
-//onMounted( () => {
-  const endDate = new Date(scheduleDate)
-  endDate.setTime(scheduleDate.getTime()+(3*60*60*1000))
-  windowDate.value = [scheduleDate, endDate]
-//})
+const endDate = new Date(scheduleDate)
+endDate.setTime(scheduleDate.getTime()+(3*60*60*1000))
+windowDate.value = [scheduleDate, endDate]
+
+const save = ( ()=>{
+  console.log('save')
+})
+
+const saveNew = (()=>{
+  console.log('saveNew')
+})
+
+
 </script>
 
 <template>
   <div class="container">
-    <p>Schedule window for {{scheduleDate}}</p>
+    <h1>Schedule Volunteer Window</h1>
+    <h2>{{dayjs(scheduleDate).format('dddd MM/DD/YYYY')}}  {{dayjs(windowDate[0]).format('h:mm A')}} - {{dayjs(windowDate[1]).format('h:mm A')}}</h2>
 
     <form >
       <div v-if="showSaveMessage" class="text-bg-success">{{saveMessage}}</div>
       <div v-if="showDeleteMessage" class="text-bg-danger">{{deleteMessage}}</div>
+
+      <div class="row my-3">
+        <div class="col">
+          <label class="form-label" for="location">Location</label>
+          <select id="location" class="form-select" >
+            <option value="1">NCFB</option>
+            <option value="2">The Shack - Ardmore Community Center</option>
+          </select>
+        </div>
+      </div>
 
       <div class="row my-3">
         <div class="col">
@@ -41,7 +61,6 @@ const windowDate = ref()
           </select>
         </div>
       </div>
-
 
       <div class="row my-3">
         <div class="col">
@@ -57,15 +76,21 @@ const windowDate = ref()
         </div>
       </div>
 
-
       <div class="row my-3">
         <div class="col">
           <label class="form-label" for="a">Time Window</label>
           <vue-date-picker v-model="windowDate" range></vue-date-picker>
-
         </div>
       </div>
 
+      <div class="row my-3">
+        <div class="col">
+          <button class="btn btn-primary btn-sm" @click.prevent="save">Save</button>
+          <button class="mx-3 btn btn-primary btn-sm" @click.prevent="saveNew">Save & New</button>
+        </div>
+        <div class="col">
+        </div>
+      </div>
 
     </form>
     </div>
