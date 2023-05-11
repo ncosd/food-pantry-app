bf96217962521a4013394e64abae79b658eed242
Darrel Herbst <dherbst@gmail.com>
2023-05-11 16:33:49 -0400

    Enabled approve/ignore buttons

--- web/admin/src/components/VolunteerList.vue
+++ web/admin/src/components/VolunteerList.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { getFirestore, collection, doc, getDoc, updateDoc } from 'firebase/firestore'
+
 const props = defineProps({
   volunteers: Array,
 })
@@ -11,6 +13,20 @@ const formatDate = (t) => {
 
 const niy = ( () => {
   alert('Not implemented yet.')
+})
+
+const updateApprove = ( async (id) => {
+  const db = getFirestore()
+  const vProfileStateRef = await doc(db, 'volunteerprofilestate', id)
+  await updateDoc(vProfileStateRef, {'status': 'active'})
+  console.log('approved' + id)
+})
+
+const updateIgnore = ( async (id) => {
+  const db = getFirestore()
+  const vProfileStateRef = await doc(db, 'volunteerprofilestate', id)
+  await updateDoc(vProfileStateRef, {'status': 'inactive'})
+  console.log('inactive ' + id)
 })
 
 </script>
@@ -36,8 +52,8 @@ const niy = ( () => {
       <td><router-link :to="{ name: 'Profile', params:{ uid: v.userid}}">{{v.email}}</router-link></td>
       <td>{{v.status}}</td>
       <td>
-        <button class="btn btn-sm btn-primary me-1" @click="niy">Approve</button>
-        <button class="btn btn-sm btn-secondary" @click="niy">Ignore</button>
+        <button class="btn btn-sm btn-primary me-1" @click="updateApprove(v.userid)">Approve</button>
+        <button class="btn btn-sm btn-secondary" @click="updateIgnore(v.userid)">Ignore</button>
       </td>
       <td>{{formatDate(v.updated)}}</td>
     </tr>
