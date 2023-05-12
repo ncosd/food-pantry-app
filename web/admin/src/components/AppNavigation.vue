b3442d707460ad1e98fa18aca21af4a0ec7a3c2d
Darrel Herbst <dherbst@gmail.com>
2023-05-12 15:56:26 -0400

    Moving layout

--- web/admin/src/components/AppNavigation.vue
+++ web/admin/src/components/AppNavigation.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
-      <a class="navbar-brand" href="/">{{config.adminAppNavName}}</a>
+      <a class="navbar-brand" href="/"><img src="/ncfb-logo.png" :alt="config.adminAppNavName" :title="config.adminAppNavName"></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
