65a5618718517f95185bc881a57e0e3210b89efc
Darrel Herbst <dherbst@gmail.com>
2023-05-24 16:38:31 -0400

    Save attending

--- web/admin/src/views/VolunteerWindow.vue
+++ web/admin/src/views/VolunteerWindow.vue
@@ -2,7 +2,7 @@
 import { ref, defineProps, onBeforeMount } from 'vue'
 import { useRouter } from 'vue-router'
 import { useAuthUserStore } from '@/stores/authUser'
-import { collection, getFirestore, query, where, doc, getDoc, addDoc, updateDoc, deleteDoc } from 'firebase/firestore'
+import { collection, getFirestore, query, where, doc, getDoc, setDoc, updateDoc, deleteDoc } from 'firebase/firestore'
 import dayjs from 'dayjs'
 
 const props = defineProps({
@@ -11,7 +11,9 @@ const props = defineProps({
 const user = useAuthUserStore()
 var volWindow = ref()
 var errMsg = ref()
-
+const successMsg = ref()
+const isSignedUp = ref(false)
+const signedUpMessage = ref('not signed up')
 
 onBeforeMount( async () => {
   if (props.id) {
@@ -27,6 +29,22 @@ onBeforeMount( async () => {
     }
   }
 })
+
+
+const signUp = (async () => {
+  successMsg.value = ''
+
+  console.log('user.data.id=' + JSON.stringify(user.data))
+
+  const db = getFirestore()
+  const attendRef = doc(db, 'window', props.id, 'attending', user.data.uid)
+  const setMerge = await setDoc(attendRef, { name: user.data.displayName, mod: new Date()}, {update: true})
+
+
+  isSignedUp.value = true
+  signedUpMessage.value = 'signed up'
+  successMsg.value = 'You have signed up!'
+})
 </script>
 
 <template>
@@ -35,17 +53,24 @@ onBeforeMount( async () => {
   <template v-if="errMsg">
     <div class="text-bg-danger">{{errMsg}}</div>
   </template>
+  <template v-if="successMsg">
+    <div class="text-bg-success">{{successMsg}}</div>
+  </template>
   <template v-if="volWindow && volWindow.starttime">
-    <div>Date: {{ dayjs(volWindow.starttime.toDate()).format('dddd MM/DD/YYYY h:mm A') }} - {{ dayjs(volWindow.endtime.toDate()).format('h:mm A') }}</div>
+    <div :class="['badge', {'text-bg-success':isSignedUp}, {'text-bg-secondary':!isSignedUp}]">You are {{signedUpMessage}}</div>
+    <div>{{ dayjs(volWindow.starttime.toDate()).format('dddd MM/DD/YYYY') }}</div>
+    <div>{{ dayjs(volWindow.starttime.toDate()).format('h:mm A') }} - {{ dayjs(volWindow.endtime.toDate()).format('h:mm A') }}</div>
     <div>Volunteers Needed: {{ volWindow.numNeeded }}</div>
     <div>Location: {{ volWindow.location }} <i class="bi bi-geo-alt-fill"></i> </div>
     <div>Activity: {{ volWindow.tasktype }}</div>
     <div class="my-3">
-      <button class="btn btn-sm btn-primary">Sign up</button>
+      <button class="btn btn-sm btn-primary" @click.prevent="signUp">Sign up</button>
       <button class="btn btn-sm btn-danger mx-3">Cancel</button>
     </div>
+    <div>
+      <button class="btn btn-sm"><i class="bi bi-calendar"></i> Add to Calendar</button>
+    </div>
+    <div>[disclaimertext]</div>
   </template>
-
 </div>
-
 </template>
