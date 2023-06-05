044e554b974d3f5d5c170d6bfae5d5f8e0ae257d
Biraj Meshiya <biraj.meshiya16@gmail.com>
2023-06-05 13:32:15 -0400

    added disclaimer notification

---
+++ web/admin/src/components/VolunteersWindowDisclaimer.vue
@@ -0,0 +1,10 @@
+<script setup>
+import  { ref } from 'vue'
+</script>
+
+<template>
+  <div class="container">
+	<p1>Please keep in mind that the Food Bank’s needs are constantly changing so it is not guaranteed that the task you</p1>
+	<p2>have signed up for is what you will be asked to do.</p2>
+  </div> 
+</template>
