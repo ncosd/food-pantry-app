1a35bbf3f8915efc2826f0103acb876e8bb28948
Darrel Herbst <dherbst@gmail.com>
2023-10-02 14:34:23 -0400

    Added calendar link in navbar.  Commented out delivery.

--- web/app/src/components/DeliveryExplainer.vue
+++ web/app/src/components/DeliveryExplainer.vue
@@ -7,10 +7,12 @@ import { config } from '@/config';
     <div class="card-body">
       <h5 class="card-title">Food Deliveries</h5>
       <div class="card-text">
-        <p>Applications for food delivery are currently being reviewed.</p>
+        <p>Applications for food delivery are currently on hold.</p>
         <p>Our service area is {{config.DeliveryAreaNames}}</p>
+        <!--
         <p>If you are in need of assistance and live within our service area, fill out the <a :href="config.DeliveryFormURL">delivery application</a> to apply.
         </p>
+        -->
       </div>
     </div>
   </div>
