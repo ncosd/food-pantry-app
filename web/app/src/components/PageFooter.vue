cfdfc22d8e8471ab6ef3fc0ba3e2ddd768960437
Darrel Herbst <dherbst@gmail.com>
2023-10-04 14:22:38 -0400

    Adding version to page footer

--- web/app/src/components/PageFooter.vue
+++ web/app/src/components/PageFooter.vue
@@ -1,3 +1,7 @@
+<script setup>
+import { config } from '@/config'
+</script>
+
 <template>
 <footer class="footer my-3">
  <ul class="list-group list-group-horizontal border border-0">
@@ -5,13 +9,7 @@
    <li class="list-group-item border border-0"><router-link to="/privacy-policy">Privacy Policy</router-link></li>
    <li class="list-group-item border border-0"><router-link to="/contact"><i class="bi bi-telephone-fill"></i> Contact</router-link></li>
    <li class="list-group-item border border-0"><a href="https://github.com/ncosd/food-pantry-app"><i class="bi bi-github"></i> Made with Food-Pantry-App</a></li>
+   <li class="list-group-item border border-0">{{config.version}}</li>
  </ul>
 </footer>
 </template>
-
-<script setup>
-
-</script>
-
-<style>
-</style>
