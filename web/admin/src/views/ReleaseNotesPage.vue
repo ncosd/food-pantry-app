2a57972f29edbb5d10b8be2f53680e44ca4080eb
Darrel Herbst <dherbst@gmail.com>
2023-10-04 16:23:09 -0400

    Set up release notes for next version.

--- web/admin/src/views/ReleaseNotesPage.vue
+++ web/admin/src/views/ReleaseNotesPage.vue
@@ -5,6 +5,11 @@
 <template>
 <div class="container">
   <h1>Release Notes</h1>
+  <h2 id="1.3.0">October 11, 2023 - 1.3.0</h2>
+  <ul>
+    <li>Admin - Adding calendar next/prev month to schedule page.</li>
+    <li>Admin - sorting volunteer list by first name.</li>
+  </ul>
   <h2 id="1.2.0">October 4, 2023 - 1.2.0</h2>
   <ul>
     <li>Volunteer Unavailability days are displayed on the Calendar page.</li>
