3f3bd75c4963c7366d20434736a81eb6f5f6c381
Darrel Herbst <dherbst@gmail.com>
2023-12-27 09:45:43 -0500

    Prepare release 1.8.0

--- web/admin/src/config.js
+++ web/admin/src/config.js
@@ -1,5 +1,5 @@
 export let config = {
-  version: "1.7.0",
+  version: "1.8.0",
   adminAppNavName: import.meta.env.VITE_ADMIN_APP_NAV_NAME || 'ADMIN_APP_NAV_NAME',
   adminAppNavImg: import.meta.env.VITE_ADMIN_APP_NAV_IMG || null,
   appNavName: import.meta.env.VITE_APP_NAV_NAME || 'APP_NAV_NAME',
--- web/admin/src/views/ReleaseNotesPage.vue
+++ web/admin/src/views/ReleaseNotesPage.vue
@@ -5,6 +5,10 @@
 <template>
 <div class="container">
   <h1>Release Notes</h1>
+  <h2 id="1.8.0">January , 2024 - 1.8.0</h2>
+  <ul>
+    <li><a href="https://github.com/ncosd/food-pantry-app/issues/">#</a> </li>
+  </ul>
   <h2 id="1.7.0">December 27, 2023 - 1.7.0</h2>
   <ul>
     <li><a href="https://github.com/ncosd/food-pantry-app/issues/215">#215</a> Create color key for the volunteer window calendar.</li>
