157fd87c5de918fd756253b067e1a500dad302e5
sam <samulla13@gmail.com>
2024-01-02 21:53:08 +0530

    added sorting to volunteers list

---
+++ web/admin/src/components/SortableTableHeader.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { defineEmits, defineProps } from "vue"
+
+const emit = defineEmits(["sort-list"])
+const props = defineProps({
+  heading: String,
+  sortKey: String,
+  sortBy: String,
+  sortAsc: Boolean,
+})
+</script>
+
+<template>
+  <th @click="emit('sort-list', sortKey)">
+    <div class="d-flex gap-2" style="cursor: pointer" title="sort">
+      <span>{{ heading }}</span>
+      <i v-if="sortBy === sortKey" :class="['bi', { 'bi-sort-down-alt': sortAsc, 'bi-sort-up': !sortAsc }]" />
+    </div>
+  </th>
+</template>
--- web/admin/src/views/VolunteersPage.vue
+++ web/admin/src/views/VolunteersPage.vue
@@ -1,40 +1,49 @@
 <script setup>
-import { reactive, computed, ref, onBeforeMount } from 'vue'
-import VolunteerList from '@/components/VolunteerList.vue'
-import { collection, getFirestore, query, where, getDocs, orderBy } from 'firebase/firestore';
+import { reactive, computed, ref, onBeforeMount } from "vue"
+import VolunteerList from "@/components/VolunteerList.vue"
+import { collection, getFirestore, query, where, getDocs, orderBy } from "firebase/firestore"
 
 var volunteers = ref()
-const statusFilter = ref('in-review')
+const statusFilter = ref("in-review")
+const sortBy = ref("firstname")
+const sortAsc = ref(true)
 
-const refreshList = ( async () => {
+const refreshList = async () => {
   const db = getFirestore()
-  const q = query(collection(db, 'volunteerprofilestate'), where('status', '==', statusFilter.value), orderBy('firstname'))
+  const q = query(collection(db, "volunteerprofilestate"), where("status", "==", statusFilter.value), orderBy(sortBy.value, sortAsc.value ? "asc" : "desc"))
   const pstates = await getDocs(q)
   volunteers.value = []
-  pstates.forEach((prof)=> {
+  pstates.forEach(prof => {
     volunteers.value.push(prof.data())
   })
-})
+}
 
-onBeforeMount( async () => {
+const sortList = param => {
+  console.log(param)
+  sortAsc.value = sortBy.value === param ? !sortAsc.value : true
+  sortBy.value = param
+  refreshList()
+}
+
+onBeforeMount(async () => {
   await refreshList()
 })
 </script>
 
 <template>
-<div class="container">
-  <div class="row ms-1">
-    <b>Filter</b>
-    <div class="col">
-      <label class="col-form-label" for="statusFilter">Status</label>
-    <select class="form-select" id="statusFilter" v-model="statusFilter" @change="refreshList">
-    <option value='in-review'>in-review</option>
-    <option value='active'>active</option>
-    <option value='inactive'>inactive</option>
-    </select>
+  <div class="container">
+    <div class="row ms-1">
+      <b>Filter</b>
+      <div class="col">
+        <label class="col-form-label" for="statusFilter">Status</label>
+        <select class="form-select" id="statusFilter" v-model="statusFilter" @change="refreshList">
+          <option value="in-review">in-review</option>
+          <option value="active">active</option>
+          <option value="inactive">inactive</option>
+        </select>
+      </div>
     </div>
-  </div>
 
-  <volunteer-list :volunteers=volunteers @refresh-list="refreshList"></volunteer-list>
-</div>
+    <volunteer-list :volunteers="volunteers" :sortBy="sortBy" :sortAsc="sortAsc" @refresh-list="refreshList" @sort-list="sortList"></volunteer-list>
+  </div>
 </template>
