4f683c18bde428a085c60591f3802d41faba0476
sam <samulla13@gmail.com>
2024-01-02 23:34:03 +0530

    fix for column size changing

--- web/admin/src/components/SortableTableHeader.vue
+++ web/admin/src/components/SortableTableHeader.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { defineEmits, defineProps } from "vue"
+import { computed, defineEmits, defineProps } from "vue"
 
 const emit = defineEmits(["sort-list"])
 const props = defineProps({
@@ -8,13 +8,15 @@ const props = defineProps({
   sortBy: String,
   sortAsc: Boolean,
 })
+
+const isActive = computed(() => props.sortBy === props.sortKey)
 </script>
 
 <template>
   <th @click="emit('sort-list', sortKey)">
     <div class="d-flex gap-2" style="cursor: pointer" title="sort">
       <span>{{ heading }}</span>
-      <i v-if="sortBy === sortKey" :class="['bi', { 'bi-sort-down-alt': sortAsc, 'bi-sort-up': !sortAsc }]" />
+      <i :class="['bi', { 'bi-sort-down-alt': sortAsc, 'bi-sort-up': !sortAsc, visible: isActive, invisible: !isActive }]" />
     </div>
   </th>
 </template>
