c547f5cd5317c386ed1cb0af4c2f3aa12812696e
Darrel Herbst <dherbst@gmail.com>
2024-01-03 13:51:40 -0500

    Adds SignaturePad to the Agreement page.

--- web/admin/src/components/AgreementForm.vue
+++ web/admin/src/components/AgreementForm.vue
@@ -1,19 +1,30 @@
 <script setup>
 import { ref } from 'vue'
 import dayjs from 'dayjs'
+import { VueSignaturePad } from 'vue-signature-pad'
 
 const props = defineProps({
   agreementHTML: String,
   name: String,
   agreementId: String,
 })
+
+const signaturePad = ref(null)
+
+const saveForm = ()=>{
+  console.log('saveForm')
+  console.log('signaturePad', signaturePad.value.isEmpty())
+  const { isEmpty, data } = signaturePad.value.saveSignature()
+  console.log('isEmpty', isEmpty, 'data', data)
+
+}
 </script>
 
 <template>
   <div class="container">
     <div v-html="props.agreementHTML"></div>
 
-    <form>
+    <form @submit.prevent="saveForm">
       <div class="my-3">
         <input type="checkbox" class="form-check-input me-3" id="agreementCheck">
         <label class="form-check-label" for="agreementCheck">By checking this, I have read and I agree with all of the above.</label>
@@ -28,7 +39,12 @@ const props = defineProps({
         <label class="form-label" for="agreementSignature">
           Signature of Staff Member/Volunteer
         </label>
-        <input type="text" class="form-control" id="agreementSignature" />
+
+
+        <VueSignaturePad id="agreementSignature" class="border" ref="signaturePad"/>
+        <div id="signatureHelp" class="form-text">Draw your signature in the box above with your mouse or finger</div>
+
+
       </div>
 
       <div class="my-3">
@@ -36,7 +52,7 @@ const props = defineProps({
           <input type="text" class="form-control" id="printedName" />
       </div>
       <div class="my-3">
-        <button type="submit" class="btn btn-primary" disabled>Submit</button>
+        <button type="submit" class="btn btn-primary">Submit</button>
       </div>
     </form>
   </div>
