7d4d43bef149083f84f3887449bca075da143e99
Darrel Herbst <dherbst@gmail.com>
2024-01-03 09:08:42 -0500

    Add margin to form sections

--- web/admin/src/components/AgreementForm.vue
+++ web/admin/src/components/AgreementForm.vue
@@ -14,28 +14,28 @@ const props = defineProps({
     <div v-html="props.agreementHTML"></div>
 
     <form>
-      <div>
+      <div class="my-3">
         <input type="checkbox" class="form-check-input me-3" id="agreementCheck">
         <label class="form-check-label" for="agreementCheck">By checking this, I have read and I agree with all of the above.</label>
       </div>
 
-      <div>
+      <div class="my-3">
           <label class="form-label" for="agreementDate">Date</label>
           <input type="text" class="form-control" id="agreementDate" :value="dayjs().toDate()"/>
       </div>
 
-      <div>
+      <div class="my-3">
         <label class="form-label" for="agreementSignature">
           Signature of Staff Member/Volunteer
         </label>
         <input type="text" class="form-control" id="agreementSignature" />
       </div>
 
-      <div>
+      <div class="my-3">
           <label class="form-label" for="printedName">Printed Name</label>
           <input type="text" class="form-control" id="printedName" />
       </div>
-      <div class="mt-3">
+      <div class="my-3">
         <button type="submit" class="btn btn-primary" disabled>Submit</button>
       </div>
     </form>
