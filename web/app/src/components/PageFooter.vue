88cd20dd179ee0603e6938956e093673cd17a771
Riya Rakshit <rbr.rakshit14@gmail.com>
2024-01-09 20:53:35 +0530

    Adding release notes for the guest app

--- web/app/src/components/PageFooter.vue
+++ web/app/src/components/PageFooter.vue
@@ -9,7 +9,7 @@ import { config } from '@/config'
    <li class="list-group-item border border-0"><router-link to="/privacy-policy">Privacy Policy</router-link></li>
    <li class="list-group-item border border-0"><router-link to="/contact"><i class="bi bi-telephone-fill"></i> Contact</router-link></li>
    <li class="list-group-item border border-0"><a href="https://github.com/ncosd/food-pantry-app"><i class="bi bi-github"></i> Made with Food-Pantry-App</a></li>
-   <li class="list-group-item border border-0">{{config.version}}</li>
+   <li class="list-group-item border border-0"><router-link :to='{name:"ReleaseNotes", hash: "#"+config.version}'><i class="bi bi-journal-check"></i> {{config.version}}</router-link></li>
  </ul>
 </footer>
 </template>
