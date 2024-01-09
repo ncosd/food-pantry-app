aa17e2205181b3ccf01b0f324ac180030b7c21e8
Darrel Herbst <dherbst@gmail.com>
2024-01-09 12:28:18 -0500

    prep release 1.11.0

--- web/admin/src/views/ReleaseNotesPage.vue
+++ web/admin/src/views/ReleaseNotesPage.vue
@@ -5,6 +5,10 @@
 <template>
 <div class="container">
   <h1>Release Notes</h1>
+  <h2 id="1.11.0">January , 2024 - 1.11.0</h2>
+  <ul>
+    <li></li>
+  </ul>
   <h2 id="1.10.0">January 9, 2024 - 1.10.0</h2>
   <ul>
     <li><a href="https://github.com/ncosd/food-pantry-app/issues/181">#181</a> Adds signable confidentiality agreement page.</li>
