1476acae2c5746c5e2f431ec24b4ed0e3b5fa7b8
Darrel Herbst <dherbst@gmail.com>
2024-01-10 09:08:50 -0500

    Adds Pronouns to the profile.

--- web/admin/src/views/ReleaseNotesPage.vue
+++ web/admin/src/views/ReleaseNotesPage.vue
@@ -7,6 +7,7 @@
   <h1>Release Notes</h1>
   <h2 id="1.11.0">January , 2024 - 1.11.0</h2>
   <ul>
+    <li><a href="https://github.com/ncosd/food-pantry-app/issues/238">#238</a> Add pronouns to the profile.</li>
     <li>Improved "make release" automation.</li>
     <li>Clarified how to run the Guest and Volunteer apps on the README.md file.</li>
   </ul>
