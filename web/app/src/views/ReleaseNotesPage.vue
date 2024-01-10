820e9fb18c4055860057d40f70479e9f221a7d7a
Darrel Herbst <dherbst@gmail.com>
2024-01-09 23:53:17 -0500

    Adds release note for #221

--- web/app/src/views/ReleaseNotesPage.vue
+++ web/app/src/views/ReleaseNotesPage.vue
@@ -7,6 +7,7 @@
   <h1>Release Notes</h1>
   <h2 id="1.11.0">January , 2024 - 1.11.0</h2>
   <ul>
+    <li><a href="https://github.com/ncosd/food-pantry-app/issues/221">#221</a> Redirect a volunteer to the volunteer app if they login to the guest app.</li>
     <li>Clarified how to run the Guest and Volunteer apps on the README.md file.</li>
   </ul>
   <h2 id="1.10.0">January 9, 2024 - 1.10.0</h2>
