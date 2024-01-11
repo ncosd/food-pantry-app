ef9c551c51f8ef4eb531b7549470db9bbc85ea91
Darrel Herbst <dherbst@gmail.com>
2024-01-10 21:34:05 -0500

    Release 1.12.0 prep.

--- web/admin/src/views/ReleaseNotesPage.vue
+++ web/admin/src/views/ReleaseNotesPage.vue
@@ -5,6 +5,10 @@
 <template>
 <div class="container">
   <h1>Release Notes</h1>
+  <h2 id="1.12.0">January , 2024 - 1.12.0</h2>
+  <ul>
+    <li></li>
+  </ul>
   <h2 id="1.11.0">January 10, 2024 - 1.11.0</h2>
   <ul>
     <li><a href="https://github.com/ncosd/food-pantry-app/issues/238">#238</a> Add pronouns to the profile.</li>
